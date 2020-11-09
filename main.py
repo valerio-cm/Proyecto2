e3456e667f2fdd2246c648d641992a8987aa65f5
Valerio <201905743choc@gmail.com>
2020-11-08 23:08:48 -0600

    Subiendo a Heroku

--- main.py
+++ main.py
@@ -321,6 +321,5 @@ def EliminarReceta(id):
 
     
 
-#if __name__ == "__main__":
- #   app.run(host="0.0.0.0",port=3000, debug=True)
-app.run(port=3000, debug=True)    
+if __name__ == "__main__":
+    app.run(threaded=True, host="0.0.0.0",port=5000, debug=True)
